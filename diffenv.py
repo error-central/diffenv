4c87cdc50c08840e257723b9b00524c3a95971dd
Stan James <stan@wanderingstan.com>
2019-07-24 15:33:38 +0200

    sort!

--- diffenv.py
+++ diffenv.py
@@ -7,6 +7,7 @@ from os.path import isfile, join
 mypath = './facets'
 
 facetScripts = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+facetScripts.sort()
 
 diffenv = ''
 for script in facetScripts:
@@ -17,6 +18,8 @@ for script in facetScripts:
       process = subprocess.Popen(
           [join(mypath, script)], stdout=subprocess.PIPE)
       out, err = process.communicate()
+      if err:
+        sys.stderr.write(err)
       # TODO: Check if err happened. None or empty string?
       diffenv += out.decode("utf-8")
   except subprocess.CalledProcessError as e:
