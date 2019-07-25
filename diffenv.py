23bbe29083ae49c74944f7936db6ba097409e614
Stan James <stan@wanderingstan.com>
2019-07-25 22:48:37 +0200

    Run scripts in ~/.diffenv/facets

--- diffenv.py
+++ diffenv.py
@@ -15,7 +15,7 @@ def run_facet(name, path):
   """ Run a facet and return the results as string """
   result = ''
   result += '\n' + ('=' * 70) + '\n'
-  result += script
+  result += name
   result += '\n' + ('=' * 70) + '\n'
   try:
     process = subprocess.Popen([path], stdout=subprocess.PIPE)
@@ -24,21 +24,27 @@ def run_facet(name, path):
       sys.stderr.write(err)
     result += (out.decode("utf-8"))
   except subprocess.CalledProcessError as e:
-    sys.stderr.write("Problem running %s: %e" % (script, e))
-    result += "Problem running %s: %e" % (script, e)
+    sys.stderr.write("Problem running %s: %e" % (path, e))
+    result += "Problem running %s: %e" % (path, e)
   return result
 
-facet_path = './facets'
-
-facetScripts = [f for f in listdir(facet_path) if isfile(join(facet_path, f))]
-facetScripts.sort() # Sort them alphabetically
-
+# Default facets
+default_facet_dir = './facets'
+default_facets = [(default_facet_dir, f) for f in listdir(
+    default_facet_dir) if isfile(join(default_facet_dir, f))]
+# User facets
+user_facet_dir = os.path.expanduser('~/.diffenv/facets')
+user_facets = [(user_facet_dir, f) for f in listdir(user_facet_dir) if isfile(
+    join(user_facet_dir, f))] if os.path.isdir(user_facet_dir) else []
+# Sort all facets
+facets = default_facets + user_facets
+facets.sort()  # Sort them alphabetically
+# Run the facets!
 diffenv = ''
-for script in facetScripts:
-  diffenv += run_facet(script, join(facet_path, script))
-
+for (facet_dir, facet_name) in facets:
+  diffenv += run_facet(facet_name, join(facet_dir, facet_name))
 # Handle outputting to file or stdout
 outfilestream = sys.stdout if args.output is None else open(args.output, 'w')
-
+# Output our results
 print(diffenv, file=outfilestream)
 
