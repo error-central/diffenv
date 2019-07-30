112db6ef85e4bfd19aa261bcf571fd614aaa5e21
Stan James <stan@wanderingstan.com>
2019-07-30 15:49:04 +0200

    autopep8 it all!

--- diffenv/diffviewer.py
+++ diffenv/diffviewer.py
@@ -17,115 +17,116 @@ B_MARKER_TXT = '++>'
 
 
 def string_diff(fromlines, tolines):
-  diff = difflib.unified_diff(fromlines, tolines, n=8, lineterm='\n')
-  for i, line in enumerate(diff):
-    if i > 2:
-      if line.startswith('-'):
-        yield A_MARKER_TXT + line[1:]
-      elif line.startswith('+'):
-        yield B_MARKER_TXT + line[1:]
-      else:
-        yield line
+    diff = difflib.unified_diff(fromlines, tolines, n=8, lineterm='\n')
+    for i, line in enumerate(diff):
+        if i > 2:
+            if line.startswith('-'):
+                yield A_MARKER_TXT + line[1:]
+            elif line.startswith('+'):
+                yield B_MARKER_TXT + line[1:]
+            else:
+                yield line
 
 
 def display_diff(fromfile, tofile, outfilestream):
-  buf = StringIO()
-  yaml.dump(diff_nested(fromfile, tofile), buf)
-  difflines = buf.getvalue().splitlines(True)
-  outfilestream.writelines(color_diff(difflines))
+    buf = StringIO()
+    yaml.dump(diff_nested(fromfile, tofile), buf)
+    difflines = buf.getvalue().splitlines(True)
+    outfilestream.writelines(color_diff(difflines))
 
 
 def color_diff(diff):
-  marking = False
-  col = 0
-  for line in diff:
-    result = ''
-    if not marking and A_MARKER in line:
-      if B_MARKER in line:
-        # handle single-line case (if applicable?)
-        i = line.index(B_MARKER)
-        yield Fore.GREEN + line[:i] + Fore.RESET + Fore.RED + line[i:] + Fore.RESET
-      else:
-        marking = True
-        yield Fore.GREEN + line
+    marking = False
+    col = 0
+    for line in diff:
+        result = ''
+        if not marking and A_MARKER in line:
+            if B_MARKER in line:
+                # handle single-line case (if applicable?)
+                i = line.index(B_MARKER)
+                yield Fore.GREEN + line[:i] + Fore.RESET + Fore.RED + line[i:] + Fore.RESET
+            else:
+                marking = True
+                yield Fore.GREEN + line
 
-    elif marking and B_MARKER in line:
-      col = line.index(B_MARKER)
-      yield Fore.RESET + Fore.RED + line
+        elif marking and B_MARKER in line:
+            col = line.index(B_MARKER)
+            yield Fore.RESET + Fore.RED + line
 
-    elif marking and len(line) - len(line.lstrip()) < col:
-      # deindented
-      marking = False
-      yield Fore.RESET + line
+        elif marking and len(line) - len(line.lstrip()) < col:
+            # deindented
+            marking = False
+            yield Fore.RESET + line
 
-    elif not marking and A_MARKER_TXT in line:
-      yield Fore.GREEN + line + Fore.RESET
-    elif not marking and B_MARKER_TXT in line:
-      yield Fore.RED + line + Fore.RESET
+        elif not marking and A_MARKER_TXT in line:
+            yield Fore.GREEN + line + Fore.RESET
+        elif not marking and B_MARKER_TXT in line:
+            yield Fore.RED + line + Fore.RESET
 
-    else:
-      yield line
+        else:
+            yield line
 
 
 def diff_nested(m1, m2):
-  """Computes diff of two nested YAML structures."""
-  if isinstance(m1, collections.Mapping) and m2:
-    # we are comparing dictionaries
-    result = deepcopy(m1)
-    for key in m1:
-      if m1[key] == m2.get(key):
-        del result[key]
-      else:
-        result[key] = diff_nested(m1[key], m2.get(key))
+    """Computes diff of two nested YAML structures."""
+    if isinstance(m1, collections.Mapping) and m2:
+        # we are comparing dictionaries
+        result = deepcopy(m1)
+        for key in m1:
+            if m1[key] == m2.get(key):
+                del result[key]
+            else:
+                result[key] = diff_nested(m1[key], m2.get(key))
 
-    for key in m2:
-      if key not in m1:
-        result[key] = diff_nested(None, m2[key])
-    return result
+        for key in m2:
+            if key not in m1:
+                result[key] = diff_nested(None, m2[key])
+        return result
 
-  elif isinstance(m1, collections.Sequence) and not isinstance(m1, str) and m2:
-    # we are comparing lists
-    result = deepcopy(m1)
-    if len(m1) == len(m2):
-      to_delete = []
-      for i, (item1, item2) in enumerate(zip(m1, m2)):
-        if item1 == item2:
-          to_delete.append(i)
+    elif isinstance(m1, collections.Sequence) and not isinstance(m1, str) and m2:
+        # we are comparing lists
+        result = deepcopy(m1)
+        if len(m1) == len(m2):
+            to_delete = []
+            for i, (item1, item2) in enumerate(zip(m1, m2)):
+                if item1 == item2:
+                    to_delete.append(i)
+                else:
+                    print(item1, item2)
+                    print(type(item1))
+                    result[i] = diff_nested(item1, item2)
+
+            for i in sorted(to_delete, reverse=True):
+                del result[i]
+
+            return result
+
         else:
-          print(item1, item2)
-          print(type(item1))
-          result[i] = diff_nested(item1, item2)
-
-      for i in sorted(to_delete, reverse=True):
-        del result[i]
-
-      return result
+            # if the lists are unequal size
+            # does not handle nested sequences
+            matches = difflib.SequenceMatcher(
+                None, m1, m2).get_matching_blocks()
+            m1 = result
+            m2 = deepcopy(m2)
+            for i1, i2, size in reversed(matches):
+                for i in range(size):
+                    del m1[i1]
+                    del m2[i2]
+            return CommentedMap([(A_MARKER, m1),
+                                 (B_MARKER, m2)])
 
     else:
-      # if the lists are unequal size
-      # does not handle nested sequences
-      matches = difflib.SequenceMatcher(None, m1, m2).get_matching_blocks()
-      m1 = result
-      m2 = deepcopy(m2)
-      for i1, i2, size in reversed(matches):
-        for i in range(size):
-          del m1[i1]
-          del m2[i2]
-      return CommentedMap([(A_MARKER, m1),
-                           (B_MARKER, m2)])
+        # we are comparing values (likely strings) directly
+        result = CommentedMap([(A_MARKER, m1),
+                               (B_MARKER, m2)])
+        if isinstance(m1, str) and m2:
+            fromlines = m1.splitlines()
+            tolines = m2.splitlines()
+            if min(len(fromlines), len(tolines)) > 5:
+                diff = list(string_diff(fromlines, tolines))
+                if len(diff) / (len(fromlines) + len(tolines)) < 1.0:
+                    # there is material overlap
+                    # so show string diff
+                    result = LiteralScalarString('\n'.join(diff))
 
-  else:
-    # we are comparing values (likely strings) directly
-    result = CommentedMap([(A_MARKER, m1),
-                           (B_MARKER, m2)])
-    if isinstance(m1, str) and m2:
-      fromlines = m1.splitlines()
-      tolines = m2.splitlines()
-      if min(len(fromlines), len(tolines)) > 5:
-        diff = list(string_diff(fromlines, tolines))
-        if len(diff) / (len(fromlines) + len(tolines)) < 1.0:
-          # there is material overlap
-          # so show string diff
-          result = LiteralScalarString('\n'.join(diff))
-
-    return result
+        return result
--- setup.py
+++ setup.py
@@ -4,22 +4,24 @@ from setuptools import setup
 def readme():
     with open('README.md') as f:
         return f.read()
+
+
 setup(
-  name='diffenv',
-  version='0.1',
-  author='Stan James, Gabriel Pickard',
-  author_email='wanderingstan@gmail.com, wergomat@gmail.com',
-  url='http://github.com/error-central/diffenv',
-  description='Compare development environments',
-  long_description=readme(),
-  long_description_content_type='text/markdown',
-  scripts=['bin/diffenv', 'bin/diffenv-nopaging'],
-  license='MIT',
-  packages=['diffenv'],
-  install_requires=[
-    'colorama',
-    'requests',
-  ],
-  zip_safe=False,
-  
+    name='diffenv',
+    version='0.1',
+    author='Stan James, Gabriel Pickard',
+    author_email='wanderingstan@gmail.com, wergomat@gmail.com',
+    url='http://github.com/error-central/diffenv',
+    description='Compare development environments',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    scripts=['bin/diffenv', 'bin/diffenv-nopaging'],
+    license='MIT',
+    packages=['diffenv'],
+    install_requires=[
+        'colorama',
+        'requests',
+    ],
+    zip_safe=False,
+
 )
