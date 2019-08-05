a285eba637d944f3c53450f7a5ce05e878962d33
Stan James <stan@wanderingstan.com>
2019-08-05 11:12:43 +0200

    Adding stub of unit tests

---
+++ tests/tests.py
@@ -0,0 +1,48 @@
+import unittest
+
+# Unit tests for diffenv
+#
+# To run:
+#     python3 -m unittest tests.py
+
+class TestStringMethods(unittest.TestCase):
+
+
+    def test_non_git(self):
+        """ Test running in a non-git repo """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_plain(self):
+        """ Test running with no params """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_sharing(self):
+        """ Test sharing env """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_compare_http(self):
+        """ Test comparing with remote env """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_compare_file(self):
+        """ Test comparing with a file """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+    def test_passed_config(self):
+        """ Test with passing in a cofig file """
+        # TODO
+        # self.assertEqual('foo'.upper(), 'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
