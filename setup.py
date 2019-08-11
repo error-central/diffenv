630df76abab4257f51502ae4cb3cbc1f44f6fd91
Stan James <stan@wanderingstan.com>
2019-08-11 14:12:58 +0200

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='diffenv',
-    version='0.1.1',
+    version='0.2.0',
     author='Stan James, Gabriel Pickard',
     author_email='wanderingstan@gmail.com, wergomat@gmail.com',
     url='http://github.com/error-central/diffenv',
